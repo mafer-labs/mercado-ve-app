ba9ddf246f260af3f683fc8877a3dc1789b5cb0d
Mafer Cordova <88457321+mafer-labs@users.noreply.github.com>
2026-04-25 16:18:44 -0400

    Modify product catalog and add official brands
    Updated product list and added official brands.

--- catalogo.js
+++ catalogo.js
@@ -1,13 +1,22 @@
 // catalogo.js
-// Aquí puedes agregar, quitar o modificar productos y supermercados sin miedo a romper la app.
+// Aquí puedes agregar, quitar o modificar productos, marcas y supermercados.
 
 export const catalogoOficial = [
     "Aceite Vegetal", "Arroz Blanco", "Atún en Lata", "Azúcar",
-    "Café Molido", "Caraotas Negras", "Harina de Maíz", "Harina de Trigo",
-    "Huevos (Medio Cartón)", "Huevos (Cartón 30)", "Leche en Polvo",
-    "Leche Líquida", "Mantequilla / Margarina", "Mayonesa",
-    "Pasta Corta", "Pasta Larga", "Pollo Entero", "Queso Amarillo",
-    "Queso Blanco Duro", "Sal", "Salsa de Tomate"
+    "Café Molido", "Caraotas Negras", "Carne de Res", "Cebolla", 
+    "Harina de Maíz", "Harina de Trigo", "Huevos (Medio Cartón)", 
+    "Huevos (Cartón 30)", "Leche en Polvo", "Leche Líquida", 
+    "Mantequilla / Margarina", "Mayonesa", "Pasta Corta", "Pasta Larga", 
+    "Pollo Entero", "Queso Amarillo", "Queso Blanco Duro", "Sal", 
+    "Salsa de Tomate", "Tomate"
+];
+
+export const marcasOficiales = [
+    "Sin Marca", "Alfonzo Rivas", "Cacique", "Campestre", "Cargill",
+    "Diplomático", "Doña Emilia", "Eveba", "Gallo", "Heinz", "Juana",
+    "Kelly's", "Kraft", "La Lucha", "Las Llaves", "Los Andes", "Margarita",
+    "Mary", "Mavesa", "McCormick", "Montalbán", "Nelly", "P.A.N.",
+    "Palmera", "Plumrose", "Polar", "Primor", "Vellísima", "Zulia"
 ];
 
 export const supermercadosFijos = [
