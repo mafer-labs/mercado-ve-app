726b55843df288efc53d43eceddce963738b95da
Mafer Cordova <88457321+mafer-labs@users.noreply.github.com>
2026-04-25 14:47:44 -0400

    Add catalogo.js with product and supermarket lists

---
+++ catalogo.js
@@ -0,0 +1,17 @@
+// catalogo.js
+// Aquí puedes agregar, quitar o modificar productos y supermercados sin miedo a romper la app.
+
+export const catalogoOficial = [
+    "Aceite Vegetal", "Arroz Blanco", "Atún en Lata", "Azúcar",
+    "Café Molido", "Caraotas Negras", "Harina de Maíz", "Harina de Trigo",
+    "Huevos (Medio Cartón)", "Huevos (Cartón 30)", "Leche en Polvo",
+    "Leche Líquida", "Mantequilla / Margarina", "Mayonesa",
+    "Pasta Corta", "Pasta Larga", "Pollo Entero", "Queso Amarillo",
+    "Queso Blanco Duro", "Sal", "Salsa de Tomate"
+];
+
+export const supermercadosFijos = [
+    "Gama", "Excelsior Gama", "Central Madeirense", "Unicasa", "Plaza's",
+    "Forum", "Makro", "Redvital", "Hiper Líder", "Automercados San Diego",
+    "Fresh Market", "Bio Mercados", "Luvebras", "Qué Papaya"
+];
